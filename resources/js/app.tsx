fc73b3b2abf3bd80ac9817abe06ba6d54dce6913
IrishMorales <irishdmorales@xolveit.com>
2025-02-13 05:39:59 +0800

    Replace GeistUI with ChakraUI

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,25 +3,27 @@ import '../css/app.css';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
-import { GeistProvider, CssBaseline } from '@geist-ui/core';
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { ThemeProvider } from "next-themes"
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-  title: (title) => `${title} - ${appName}`,
-  resolve: (name) =>
-    resolvePageComponent(
-        `./Pages/${name}.tsx`,
-        import.meta.glob('./Pages/**/*.tsx'),
-    ),
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.tsx`,
+            import.meta.glob('./Pages/**/*.tsx'),
+        ),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
         root.render(
-            <GeistProvider themeType='dark'>
-                <CssBaseline/>
-                <App {...props} />
-            </GeistProvider>
+            <ChakraProvider value={defaultSystem}>
+                <ThemeProvider attribute="class" disableTransitionOnChange>
+                    <App {...props} />
+                </ThemeProvider>
+            </ChakraProvider>
         );
     },
 })
--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({   
+export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
             refresh: true,
         }),
         react(),
-        tailwindcss()
+        tailwindcss(),
+        tsconfigPaths()
     ],
 });
