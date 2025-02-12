110b34a1d80e625730cf1c659e9d342c54c5adfe
IrishMorales <irishdmorales@xolveit.com>
2025-02-13 06:48:41 +0800

    Clean up comments

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,6 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\Users\UserController;
 use Illuminate\Support\Facades\Route;
 
-# TODO: Redirect / or add button to 404 to return to login/home
-# TODO: Refactor into multiple route files
 Route::group([
     'prefix' => 'auth',
     'as' => 'auth.',
