325acbedc9f846149a080cfc41195be1f8664e28
IrishMorales <irishdmorales@xolveit.com>
2025-02-12 11:28:19 +0800

    Scaffold auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
+# TODO: Redirect / to /products if authenticated, else redirect to /login
 Route::get('/', function () {
-    return view('app');
+    return Inertia::render('Auth/Login');
+});
+
+# TODO
+Route::get('/register', function () {
+    return Inertia::render('Auth/Register');
 });
