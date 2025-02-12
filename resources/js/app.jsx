8707e0c76584761cb2c6090d8827857f103f9efe
IrishMorales <irishdmorales@xolveit.com>
2025-02-12 10:15:51 +0800

    Add app title

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,7 +1,10 @@
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
 
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
 createInertiaApp({
+  title: (title) => `${title} - ${appName}`,
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
     return pages[`./Pages/${name}.jsx`]
