e72f517ad6d16725ac50fb99604656c6b76633e7
IrishMorales <irishdmorales@xolveit.com>
2025-02-13 06:57:11 +0800

    Use server side validation errors

--- resources/js/Pages/Products/Form.tsx
+++ resources/js/Pages/Products/Form.tsx
@@ -20,24 +20,24 @@ export default function ProductForm({ data, setData, submit, processing, errors 
       <form onSubmit={submit}>
         <Fieldset.Root>
           <Fieldset.Content>
-            <Field.Root invalid={errors.name ? true : false} required>
-              <Field.Label htmlFor="name">Name<Field.RequiredIndicator /></Field.Label>
+            <Field.Root invalid={errors.name ? true : false}>
+              <Field.Label htmlFor="name">Name</Field.Label>
               <Input type="text" value={data?.name} onChange={e => setData('name', e.target.value)} />
               <Field.ErrorText>{errors.name}</Field.ErrorText>
             </Field.Root>
-            <Field.Root invalid={errors.description ? true : false} required>
-              <Field.Label htmlFor="description">Description<Field.RequiredIndicator /></Field.Label>
+            <Field.Root invalid={errors.description ? true : false}>
+              <Field.Label htmlFor="description">Description</Field.Label>
               <Input type="text" value={data?.description} onChange={e => setData('description', e.target.value)} />
               <Field.ErrorText>{errors.description}</Field.ErrorText>
             </Field.Root>
-            <Field.Root invalid={errors.stock ? true : false} required>
-              <Field.Label htmlFor="stock">Stock<Field.RequiredIndicator /></Field.Label>
-              <Input type="number" value={data?.stock} min={0} onChange={e => setData('stock', e.target.value)} />
+            <Field.Root invalid={errors.stock ? true : false}>
+              <Field.Label htmlFor="stock">Stock</Field.Label>
+              <Input type="number" value={data?.stock} onChange={e => setData('stock', e.target.value)} />
               <Field.ErrorText>{errors.stock}</Field.ErrorText>
             </Field.Root>
-            <Field.Root invalid={errors.price ? true : false} required>
-              <Field.Label htmlFor="price">Price<Field.RequiredIndicator /></Field.Label>
-              <Input type="number" value={data?.price} min={0} step={0.01} onChange={e => setData('price', e.target.value)} />
+            <Field.Root invalid={errors.price ? true : false}>
+              <Field.Label htmlFor="price">Price (PHP)</Field.Label>
+              <Input type="number" value={data?.price} step={0.01} onChange={e => setData('price', e.target.value)} />
               <Field.ErrorText>{errors.price}</Field.ErrorText>
             </Field.Root>
           </Fieldset.Content>
